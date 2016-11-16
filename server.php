8cb583ecb43df182a4013602e0a3640fb040fb87
Michal Zdrojewski <code@strayobject.co.uk>
2016-11-16 11:06:54 +0000

    chore(server): moved the server file to root dir

--- server.php
+++ server.php
@@ -6,9 +6,10 @@ chdir(dirname(__DIR__));
 require_once 'vendor/autoload.php';
 
 use Aerys\{Host, Root, Router, Request, Response, function websocket};
-use Phpkanban\Board\Controller\{AddCategoryController, ListBoardsController, ShowBoardController, WsBoardController};
-use Phpkanban\App\Controller\ShowIndexController;
-use Phpkanban\Task\Controller\AddTaskController;
+use Plank\Kanban\Board\Controller\{AddCategoryController, ListBoardsController, ShowBoardController, WsBoardController};
+use Plank\Kanban\App\Controller\ShowIndexController;
+use Plank\Kanban\Task\Controller\AddTaskController;
+use Plank\Kanban\Board\Entity\BoardRepository;
 /**
  * Env
  */
@@ -23,16 +24,20 @@ const AERYS_OPTIONS = [
     //"deflateMinimumLength" => 0,
 ];
 
+$dbConn = r\connect('rdb', 28015);
+$dbConn->useDb('plank');
+
+$boardRepo = new BoardRepository($dbConn);
+
 $task = function(Request $request, Response $response, array $args) {
     $response->end(sprintf('This is your task id: %s', $args['id']));
 };
-
 
 // You can add routes in two ways:
 // using route($method, $path, $callable)
 // using method functions e.g. get($path, $callable)
 $router = new Router();
-$router->route('GET', '/', new ShowIndexController());
+$router->route('GET', '/', new ShowIndexController($boardRepo));
 $router->route('GET', '/boards/{name}', new ShowBoardController());
 $router->route('GET', '/boards/?', new ShowBoardController());
 $router->route('POST', '/boards/{boardUrl}/categories', new AddCategoryController());
@@ -43,12 +48,20 @@ $router->route("GET", "/ws/boards/{name}", websocket(new WsBoardController()));
 // $router->get('/tasks/?', $task);
 // $router->post('/tasks', $task);
 
-$rootDir = new Root(dirname(__DIR__).'/web');
+$rootDir = new Root(__DIR__.'/web');
 $host = new Host();
 $host
     ->name('localhost')
-    //->expose('*', 8899)
+    ->expose('*', 7001)
     ->expose('*', 8080)
     ->use($router)
     ->use($rootDir)
 ;
+
+// $devhost
+//     ->name('localhost')
+//     //->expose('*', 8899)
+//     ->expose('*', 7001)
+//     ->use($router)
+//     ->use($rootDir)
+// ;
