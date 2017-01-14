0639711afcf2836294cf62fbc26a350f70aa1d69
Michal Zdrojewski <code@strayobject.co.uk>
2017-01-14 01:01:16 +0000

    chore(boards): locked controller to authenticated user

--- src/Kanban/Board/Controller/ListBoardsController.php
+++ src/Kanban/Board/Controller/ListBoardsController.php
@@ -4,38 +4,33 @@ declare(strict_types=1);
 namespace Plank\Kanban\Board\Controller;
 
 use Aerys\{Request, Response};
-use Plank\Kanban\App\{Exception\ItemNotFoundException, Transformer\ExceptionTransformer};
+use Plank\Kanban\App\{Exception\ItemNotFoundException, Responder\ResponderInterface, Transformer\ExceptionTransformer};
 use Plank\Kanban\Board\{Entity\Board, Entity\BoardRepository, Transformer\BoardTransformer};
 use League\Fractal\{Manager, Resource\Collection, Resource\Item};
 
 class ListBoardsController
 {
     private $boardRepo;
-    private $outputManager;
+    private $responder;
 
-    public function __construct(BoardRepository $boardRepo, Manager $outputManager)
+    public function __construct(BoardRepository $boardRepo, ResponderInterface $responder)
     {
         $this->boardRepo = $boardRepo;
-        $this->outputManager = $outputManager;
+        $this->responder = $responder;
     }
 
     public function __invoke(Request $request, Response $response, array $args): void
     {
-        /**
-         * @todo fetch board if belongs to current user only
-         */
+        $user = $request->getLocalVar('user');
+
         try {
-            $boards = $this->boardRepo->getBoards();
+            $boards = $this->boardRepo->getBoards($user->getId());
             $resource = new Collection($boards, new BoardTransformer(), 'board');
         } catch (ItemNotFoundException $e) {
             $resource = new Item($e, new ExceptionTransformer(), 'exception');
             $response->setStatus(404);
-        } catch (\Exception $e) {
-            $resource = new Item($e, new ExceptionTransformer(), 'exception');
-            $response->setStatus(400);
         }
 
-        $data = $this->outputManager->createData($resource)->toJson();
-        $response->end($data);
+        $this->responder->createAndFinish($response, $resource);
     }
 }
